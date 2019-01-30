10d16217f61090a04dce5d6fd33014b7e0215049
Marco Obermeyer <mo@obcon.de>
2019-01-30 09:37:45 +0100

    init

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,14 @@ cd $(dirname $0)
 cd ..
 BASE=$(pwd)
 
-mkdir ${BASE}/target
-pip install -t ${BASE}/target -r ${BASE}/source/requirements.txt
-cp -r ${BASE}/source/*.py ${BASE}/target
-cd ${BASE}/target
-zip -r ${BASE}/target.zip *
+PACKAGE_DIR="${BASE}/target"
+UPLOAD_DIR="${BASE}/upload"
+SOURCE_DIR="${BASE}/source"
+
+mkdir -p ${PACKAGE_DIR}
+mkdir -p ${UPLOAD_DIR}
+
+pip install -t ${PACKAGE_DIR} -r ${SOURCE_DIR}/requirements.txt
+cp -r ${SOURCE_DIR}/source/*.py ${PACKAGE_DIR}
+cd ${PACKAGE_DIR}
+zip -r ${UPLOAD_DIR}/target.zip *
