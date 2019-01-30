8683f1d34e391933b4b181fc63768ea549c6c80a
Marco Obermeyer <mo@obcon.de>
2019-01-30 11:26:13 +0100

    terraform

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,17 +12,25 @@ source ${BASE}/project.env
 
 mkdir -p ${UPLOAD_DIR}
 
+# package code
+
 mkdir -p ${PACKAGE_DIR}
 pip install -t ${PACKAGE_DIR} -r ${SOURCE_DIR}/python/requirements.txt
 rsync -avvP ${SOURCE_DIR}/python/ ${PACKAGE_DIR}
 cd ${PACKAGE_DIR}
 zip -r ${UPLOAD_DIR}/aws-cloudwatch-log-cleaner_${VERSION}_code.zip *
 rm -rf ${PACKAGE_DIR}
+cd ${BASE}
+
+# package terraform
 
 mkdir -p ${PACKAGE_DIR}
 rsync -avvP ${SOURCE_DIR}/terraform/ ${PACKAGE_DIR}
 cd ${PACKAGE_DIR}
 zip -r ${UPLOAD_DIR}/aws-cloudwatch-log-cleaner_${VERSION}_module.zip *
 rm -rf ${PACKAGE_DIR}
+cd ${BASE}
+
+# list artefacts
 
 ls -lh ${UPLOAD_DIR}
